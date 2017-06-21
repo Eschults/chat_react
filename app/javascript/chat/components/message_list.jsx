c787547157502e955d44634c1b9358b11844e440
Edward Schults <edward.schults@gmail.com>
2017-06-22 00:50:26 +0100

    ajax to fetch messages in componentDidMount

--- app/javascript/chat/components/message_list.jsx
+++ app/javascript/chat/components/message_list.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import Message from './message';
+import axios from 'axios';
 
 class MessageList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`/messages.json`)
+          .then(res => {
+            debugger
+            const messages = res.data.messages;
+            this.setState({ messages });
+          });
+  }
+
   renderMessages() {
-    return this.props.messages.map((message) => {
-      return <Message key={message.id} message={message} />;
+    return this.state.messages.map((message) => {
+      return <Message message={message} />;
     })
   }
 
