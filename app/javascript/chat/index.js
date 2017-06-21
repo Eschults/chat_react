56ede680e7a0d805e1163dd44dca4c7a0805f76b
Edward Schults <edward.schults@gmail.com>
2017-06-21 19:27:08 +0100

    added chat index.js

---
+++ app/javascript/chat/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chat from './components/chat';
+
+var chat = document.querySelector('#chat')
+
+if (chat) {
+  ReactDOM.render(<Chat />, chat);
+}
