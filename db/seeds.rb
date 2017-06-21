ee706298ac445a0376acfad5475f172ad2483bdd
Edward Schults <edward.schults@gmail.com>
2017-06-21 17:29:07 +0100

    added fields to users and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+boris = User.create!(email: "boris@lewagon.com", password: "azertyuiop", first_name: "Boris", avatar_url: "https://avatars.githubusercontent.com/u/2471555?v=3")
+seb = User.create!(email: "seb@lewagon.com", password: "azertyuiop", first_name: "Seb", avatar_url: "https://avatars.githubusercontent.com/u/414418?v=3")
+romain = User.create!(email: "romain@lewagon.com", password: "azertyuiop", first_name: "Romain", avatar_url: "https://avatars.githubusercontent.com/u/6377103?v=3")
+kevin = User.create!(email: "kevin@lewagon.com", password: "azertyuiop", first_name: "Kevin", avatar_url: "https://avatars0.githubusercontent.com/u/9978111?v=3&s=460")
+edward = User.create!(email: "edward@lewagon.com", password: "azertyuiop", first_name: "Edward", avatar_url: "https://graph.facebook.com/v2.6/10153945596946275/picture?width=64&height=64")
