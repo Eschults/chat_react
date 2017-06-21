8dc7110a55458f2d074d7c1e7cf70d8b3cf5ca17
Edward Schults <edward.schults@gmail.com>
2017-06-22 00:49:34 +0100

    axios to fetch messages

--- app/javascript/chat/components/chat.jsx
+++ app/javascript/chat/components/chat.jsx
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
 import MessageList from './message_list';
+import MessageForm from './message_form';
 
 class Chat extends Component {
   render() {
-    var messages = [{
-      id: 1,
-      user: { id: 1, first_name: "Boris" },
-      content: 'Hello'
-    },
-    {
-      id: 2,
-      user: { id: 2, first_name: "Seb" },
-      content: 'Hello'
-    },
-    {
-      id: 3,
-      user: { id: 3, first_name: "Romain" },
-      content: 'Hello'
-    }];
-
     return (
       <div>
-        <MessageList messages={messages} />
+        <MessageList />
+        <MessageForm />
       </div>
     );
   }
