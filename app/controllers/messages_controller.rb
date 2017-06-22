fa26cc084fdb919f28096a6054ef5a8ee2ef6769
Edward Schults <edward.schults@gmail.com>
2017-06-22 01:31:13 +0100

    messages create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
   def index
     @messages = Message.all
     respond_to do  |format|
@@ -8,5 +10,8 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.create!(user: current_user, content: params[:content])
+    ActionCable.server.broadcast("messages", { event: "New message" })
+    head :no_content
   end
 end
