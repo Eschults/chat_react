6765f81bc5baf355894dad3bde42ec550911b1e6
Edward Schults <edward.schults@gmail.com>
2017-06-22 01:32:14 +0100

    pass current user id

--- app/javascript/chat/index.js
+++ app/javascript/chat/index.js
@@ -5,5 +5,5 @@ import Chat from './components/chat';
 var chat = document.querySelector('#chat');
 
 if (chat) {
-  ReactDOM.render(<Chat />, chat);
+  ReactDOM.render(<Chat currentUserId={chat.dataset.currentUserId}/>, chat);
 }
