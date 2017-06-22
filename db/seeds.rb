356b94538af91f2e84be477be5446777aba6b455
Edward Schults <edward.schults@gmail.com>
2017-06-22 01:30:54 +0100

    seed'

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@ if User.count == 0
   edward = User.create!(email: "edward@lewagon.com", password: "azertyuiop", first_name: "Edward", avatar_url: "https://graph.facebook.com/v2.6/10153945596946275/picture?width=64&height=64")
 end
 
-Message.create!(user: User.find_by(first_name: "Boris"), content: "Hello!")
-Message.create!(user: User.find_by(first_name: "Seb"), content: "Hey Boris!")
-Message.create!(user: User.find_by(first_name: "Romain"), content: "Hello guys!")
+if Message.count == 0
+  Message.create!(user: User.find_by(first_name: "Boris"), content: "Hello!")
+  Message.create!(user: User.find_by(first_name: "Seb"), content: "Hey Boris!")
+  Message.create!(user: User.find_by(first_name: "Romain"), content: "Hello guys!")
+end
